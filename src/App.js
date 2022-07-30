a884f63738f0e52d9e86ef4bdba434694e123499
Timothy Parker <tlparker71@gmail.com>
2022-07-30 11:27:46 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
@@ -8,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main title="title" imageURL="imagURL" description="description" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class Footer extends Component {
     constructor() {
